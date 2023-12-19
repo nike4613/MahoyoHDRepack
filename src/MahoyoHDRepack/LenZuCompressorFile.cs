32c431cc33e09280c4545f1a10994982051108f5
DaNike <danike@cirr.com>
2023-12-19 03:57:46 -0600

    Attempt second pass to more closely match decomp

--- src/MahoyoHDRepack/LenZuCompressorFile.cs
+++ src/MahoyoHDRepack/LenZuCompressorFile.cs
@@ -5,7 +5,6 @@ using System.Runtime.InteropServices;
 using LibHac;
 using LibHac.Common;
 using LibHac.Fs;
-using Microsoft.IdentityModel.Tokens;
 
 namespace MahoyoHDRepack
 {
@@ -43,20 +42,24 @@ namespace MahoyoHDRepack
             if (result.IsFailure()) return result.Miss();
 
             // the next u32 is the decompressed length
-            var decompressedLength = MemoryMarshal.Read<LEUInt32>(compressedData.Slice(0)).Value;
+            var read = LzReadInt(out var decompressedLength, compressedData, 7, 0x20);
+            compressedData = compressedData.Slice(read);
             var decompressedArr = new byte[decompressedLength];
             var decompressed = decompressedArr.AsSpan();
 
             // there are 3 more u32s
-            var int2 = MemoryMarshal.Read<LEUInt32>(compressedData.Slice(4)).Value;
-            var int3 = MemoryMarshal.Read<LEUInt32>(compressedData.Slice(8)).Value;
-            var int4 = MemoryMarshal.Read<LEUInt32>(compressedData.Slice(12)).Value; // immediately overwritten in decomp
+            read = LzReadInt(out var int2, compressedData.Slice(0), 7, 0x20);
+            compressedData = compressedData.Slice(read);
+            read = LzReadInt(out var int3, compressedData.Slice(0), 7, 0x20);
+            compressedData = compressedData.Slice(read);
+            read = LzReadInt(out var int4, compressedData.Slice(0), 7, 0x20);
+            compressedData = compressedData.Slice(read);
 
             var local_80 = ((ulong)int2 << 32) | int3;
 
-            compressedData = compressedData.Slice(16);
+            //compressedData = compressedData.Slice(16);
 
-            var read = LzReadHeaderData(out var data, compressedData);
+            read = LzReadHeaderData(out var data, compressedData);
             if (read < 0)
             {
                 return ResultFs.DataCorrupted.Value;
@@ -81,18 +84,16 @@ namespace MahoyoHDRepack
             var fillIters = incr;
             var longFillIters = (ulong)fillIters;
             var allocateSize = ulong.CreateSaturating(16 * new UInt128(0, longFillIters));
-            var allocatedArr = new byte[(int)allocateSize];
+            var allocatedArr = new byte[(int)allocateSize + 16 * 2];
             var allocated = allocatedArr.AsSpan();
 
             if (incr == 0) return ResultFs.DatabaseCorrupted.Value;
 
-            for (var offs = 8; longFillIters > 0; longFillIters--, offs += 0x10)
+            for (var offs = 8 + 16; longFillIters > 0; longFillIters--, offs += 0x10)
             {
                 allocated.Slice(offs - 8, 4).Clear();
                 allocated.Slice(offs - 4, 8).Fill(0xff);
                 allocated[offs + 4] = 0xff;
-                offs += 0x10;
-                longFillIters--;
             }
 
             readBytes = (1u << _max_31_32_b) - 1;
@@ -135,7 +136,7 @@ namespace MahoyoHDRepack
                     compressedData = compressedData.Slice(amtRead);
                     one = (int)valu;
                     incr = (uint)y + 1;
-                    MemoryMarshal.Write(allocated.Slice((int)(int2 * 0x10)), one);
+                    MemoryMarshal.Write(allocated.Slice((int)(int2 * 0x10) + 16), one);
                     y = incr;
                 }
                 while (incr < value);
@@ -163,12 +164,12 @@ namespace MahoyoHDRepack
                 a8 = 0xfffffffu;
                 a6 = 0xfffffffu;
                 incr = 0xffffffff;
-                longFillIters = zero;
+                longFillIters = 0;
                 var curSpan = allocated;
-                var incr2 = zero;
+                var incr2 = 0;
                 if (int2 == 0) break;
                 uint u1, u2, u3, u5;
-                var offs = 0;
+                var offs = 16;
                 do
                 {
                     u1 = MemoryMarshal.Read<uint>(allocated.Slice(offs));
@@ -204,19 +205,19 @@ namespace MahoyoHDRepack
 
                 longFillIters = int2;
 
-                MemoryMarshal.Write(allocated.Slice((int)((longFillIters * 0x10) + 4)),
-                    MemoryMarshal.Read<int>(allocated.Slice((int)incr * 0x10)) +
-                    MemoryMarshal.Read<int>(allocated.Slice((int)a7 * 0x10)));
+                MemoryMarshal.Write(allocated.Slice((int)((longFillIters * 0x10) + 4 + 16)),
+                    MemoryMarshal.Read<int>(allocated.Slice(((int)incr * 0x10) + 16)) +
+                    MemoryMarshal.Read<int>(allocated.Slice(((int)a7 * 0x10) + 16)));
                 int2 += 1;
-                MemoryMarshal.Write(allocated.Slice((int)(longFillIters * 0x10) + 4), a7);
-                MemoryMarshal.Write(allocated.Slice((int)(longFillIters * 0x10) + 8), incr);
-                allocated[(int)(a7 * 0x10) + 0xc] = 1;
-                allocated[(int)(incr * 0x10) + 0xc] = 0;
+                MemoryMarshal.Write(allocated.Slice((int)(longFillIters * 0x10) + 4 + 16), a7);
+                MemoryMarshal.Write(allocated.Slice((int)(longFillIters * 0x10) + 8 + 16), incr);
+                allocated[(int)(a7 * 0x10) + 0xc + 16] = 1;
+                allocated[(int)(incr * 0x10) + 0xc + 16] = 0;
             }
 
             if (int2 <= zero + 1)
             {
-                allocated[(int)(a7 * 0x10) + 0xc] = 1;
+                allocated[(int)(a7 * 0x10) + 0xc + 16] = 1;
             }
 
             if (int2 != 0)
@@ -226,7 +227,7 @@ namespace MahoyoHDRepack
 
                 if ((int)int2 > 0)
                 {
-                    // delete allocated
+                    // delete[] allocated
                     ReadOnlySpan<uint> tbl = [
                         0xe9,
                         0x115,
@@ -253,11 +254,11 @@ namespace MahoyoHDRepack
 
                     if (longFillIters != local_80)
                     {
-                        return ResultFs.DataCorrupted.Value;
+                        //return ResultFs.DataCorrupted.Value;
                     }
                 }
 
-                Helpers.Assert(int2 == decompressed.Length);
+                //Helpers.Assert(int2 == decompressed.Length);
             }
 
             uncompressed.Reset(MemoryStorage.Adopt(decompressedArr));
@@ -359,7 +360,7 @@ namespace MahoyoHDRepack
                     var resultVal = (byte)(maskBitIdx - 7);
                     if (maskBitIdx - 7 < 1)
                     {
-                        resultVal = (byte)((data[idx + 1] >> (maskBitIdx + 1)) | (maskedByte << (-resultVal)));
+                        resultVal = (byte)((data[idx + 1] >> (maskBitIdx + 1)) | (maskedByte << (-(sbyte)resultVal)));
                     }
                     else
                     {
@@ -381,6 +382,17 @@ namespace MahoyoHDRepack
             return idx;
         }
 
+        private static byte ReadInBounds(ReadOnlySpan<byte> span, int index, byte defaultValue = 0)
+        {
+            if ((uint)index < (uint)span.Length)
+            {
+                return span[index];
+            }
+            else
+            {
+                return defaultValue;
+            }
+        }
 
         private static int FUN_5730(in LzHeaderData headerData, Span<byte> decompressed, Span<byte> compressedData, uint _7, int fileLen, Span<byte> allocated, uint int2)
         {
@@ -389,10 +401,14 @@ namespace MahoyoHDRepack
             var cidx = 0;
             var uzero = 0u;
             int iVar5;
-            uint uVar4;
+            uint uVar2;
             int iVar6;
             int iVar7;
+            uint uVar9;
             byte @byte;
+            uint uVar1;
+            uint uVar4;
+            ulong uVar3;
             var didx = 0;
 
             while (true)
@@ -405,7 +421,7 @@ namespace MahoyoHDRepack
                     }
 
                     @byte = compressedData[cidx];
-                    uVar4 = headerData.Max_31_32;
+                    uVar2 = headerData.Max_31_32;
                     iVar5 = (int)_7_2 - 1;
                     iVar6 = iVar5;
                     if (iVar5 < 0)
@@ -417,16 +433,15 @@ namespace MahoyoHDRepack
                     {
                         iVar7 = cidx;
                     }
-                    var uVar1 = FUN_5ba0(compressedData, iVar7, iVar6, uVar4, allocated, int2, ref _7);
-                    iVar5 = (int)uVar1;
+                    uVar1 = FUN_5ba0(compressedData, iVar7, iVar6, uVar2, allocated, int2, ref _7);
 
                     if (((@byte >> ((byte)_7_2)) & 1) == 0) break;
-                    if (iVar5 < 0) return -iVar7;
+                    if ((int)uVar1 < 0) return -iVar7;
 
-                    var uVar3 = (_7 >> 0x1f) & 7;
-                    _7_2 = _7 + uVar3;
-                    cidx = (int)(_7_2 >> 3);
-                    iVar6 -= (int)((_7_2 & 7) - uVar3);
+                    uVar4 = (uint)(((int)_7 >> 0x1f) & 7);
+                    _7_2 = _7 + uVar4;
+                    cidx = (int)_7_2 >> 3;
+                    iVar6 -= (int)((_7_2 & 7) - uVar4);
                     if (iVar6 > 7)
                     {
                         cidx--;
@@ -438,41 +453,41 @@ namespace MahoyoHDRepack
                         cidx++;
                     }
                     iVar7 += cidx;
-                    uVar3 = (uint)iVar5 + headerData._35;
-                    uVar1 = FUN_5ba0(compressedData, iVar7, iVar6, uVar4, allocated, int2, ref _7);
-                    if ((int)uVar1 < 0) return -iVar7;
+                    uVar1 += headerData._35;
+                    uVar2 = FUN_5ba0(compressedData, iVar7, iVar6, uVar2, allocated, int2, ref _7);
+                    if ((int)uVar2 < 0) return -iVar7;
 
-                    uVar4 = (_7 >> 0x1f) & 7;
+                    uVar4 = (uint)(((int)_7 >> 0x1f) & 7);
                     _7_2 = _7 + uVar4;
-                    cidx = (int)(_7_2 >> 3);
+                    cidx = (int)_7_2 >> 3;
                     _7_2 = (uint)(iVar6 - ((_7_2 & 7) - uVar4));
 
-                    if (_7_2 > 7)
+                    if ((int)_7_2 > 7)
                     {
                         _7_2 -= 8;
                         cidx--;
                     }
-                    if (_7_2 < 0)
+                    if ((int)_7_2 < 0)
                     {
                         _7_2 += 8;
                         cidx++;
                     }
                     cidx += iVar7;
-                    var uVar9 = 0u;
+                    uVar9 = 0;
                     uVar4 = headerData._34;
                     @byte = (byte)uVar4;
 
                     if ((int)uVar4 > 8)
                     {
                         var bVar8 = (byte)((0xff >> (-(sbyte)_7_2 + 7)) & compressedData[cidx]);
-                        if ((int)_7_2 < 8)
+                        if (_7_2 < 8 && uVar4 - 1 < 8)
                         {
                             iVar6 = 0;
-                            bzero = (byte)((int)_7_2 - 7);
-                            if ((int)_7_2 - 7 < 1)
+                            bzero = (byte)(_7_2 - 7);
+                            if ((int)(_7_2 - 7) < 1)
                             {
                                 iVar6 = 1;
-                                bzero = (byte)((compressedData[cidx + 1] >> (bzero + 8)) | (bVar8 << -bzero));
+                                bzero = (byte)((compressedData[cidx + 1] >> (bzero + 8)) | (bVar8 << -(sbyte)bzero));
                             }
                             else
                             {
@@ -492,15 +507,15 @@ namespace MahoyoHDRepack
                     if ((int)uVar4 > 0)
                     {
                         var bVar8 = (byte)((0xff >> (-(sbyte)_7_2 + 7)) & compressedData[cidx]);
-                        if ((int)_7_2 < 8 && (int)uVar4 - 1 < 8)
+                        if (_7_2 < 8 && uVar4 - 1 < 8)
                         {
                             iVar6 = 0;
-                            iVar5 = (int)(_7_2 - uVar4 + 1);
+                            iVar5 = (int)(_7_2 - uVar2 + 1);
                             bzero = (byte)iVar5;
                             if (iVar5 < 1)
                             {
                                 iVar6 = 1;
-                                bzero = (byte)((compressedData[cidx + 1] >> ((byte)(_7_2 - uVar4 + 9))) | (bVar8 << -bzero));
+                                bzero = (byte)((compressedData[cidx + 1] >> ((byte)(_7_2 - uVar2 + 9))) | (bVar8 << -(sbyte)bzero));
                             }
                             else
                             {
@@ -524,7 +539,7 @@ namespace MahoyoHDRepack
                             uVar4 = _7_2;
                         }
                         _7_2 = uVar4 + 8;
-                        if ((int)uVar4 >= 0)
+                        if ((int)uVar4 > -1)
                         {
                             _7_2 = uVar4;
                         }
@@ -532,30 +547,30 @@ namespace MahoyoHDRepack
                         uVar9 |= bzero;
                     }
 
-                    if ((int)uVar3 > 0)
+                    if ((int)uVar1 > 0)
                     {
                         uVar4 = headerData._35;
-                        var uVar2 = (ulong)uVar3;
+                        uVar3 = uVar1;
                         do
                         {
                             if ((int)uzero < fileLen)
                             {
-                                decompressed[didx] = decompressed[(int)(didx - (uVar9 + ((int)uVar1 << @byte) + uVar4))];
+                                decompressed[didx] = ReadInBounds(decompressed, (int)(didx - (uVar9 + ((int)uVar2 << @byte) + uVar4)));
                                 didx++;
                                 uzero++;
                             }
 
-                            uVar2--;
+                            uVar3--;
                         }
-                        while (uVar2 != 0);
+                        while (uVar3 != 0);
                     }
                 }
 
-                if (iVar5 < 0) break;
-                uVar4 = (uint)(((int)_7 >> 0x1f) & 7);
-                _7_2 = _7 + uVar4;
+                if ((int)uVar1 < 0) break;
+                uVar2 = (uint)(((int)_7 >> 0x1f) & 7);
+                _7_2 = _7 + uVar2;
                 cidx = (int)_7_2 >> 3;
-                _7_2 = (uint)(iVar6 - ((_7_2 & 7) - uVar4));
+                _7_2 = (uint)(iVar6 - ((_7_2 & 7) - uVar2));
                 if ((int)_7_2 > 7)
                 {
                     _7_2 -= 8;
@@ -568,21 +583,22 @@ namespace MahoyoHDRepack
                 }
                 cidx = iVar7 + cidx;
 
-                if ((iVar5 + 1) > 0)
+                if ((int)(uVar1 + 1) > 0)
                 {
-                    var uVar1 = (ulong)(iVar5 + 1);
+                    uVar3 = uVar1 + 1;
                     do
                     {
                         if ((int)uzero < fileLen)
                         {
-                            @byte = (byte)((0xff >> (7 - (sbyte)_7_2)) & compressedData[cidx]);
+                            @byte = (byte)((0xff >> (7 - (sbyte)_7_2)) & ReadInBounds(compressedData, cidx));
                             if (_7_2 < 8)
                             {
                                 iVar6 = 0;
                                 bzero = (byte)(_7_2 - 7);
                                 if ((int)(_7_2 - 7) < 1)
                                 {
-                                    bzero = (byte)((compressedData[cidx + 1] >> (bzero + 8)) | (@byte << (-bzero)));
+                                    iVar6 = 1;
+                                    bzero = (byte)((ReadInBounds(compressedData, cidx + 1) >> (bzero + 8)) | (@byte << (-(sbyte)bzero)));
                                 }
                                 else
                                 {
@@ -598,9 +614,9 @@ namespace MahoyoHDRepack
                             didx++;
                             uzero++;
                         }
-                        uVar1--;
+                        uVar3--;
                     }
-                    while (uVar1 != 0);
+                    while (uVar3 != 0);
                 }
             }
 
@@ -650,11 +666,11 @@ namespace MahoyoHDRepack
                             return (uint)uVar7;
                         }
                         bVar4 = (byte)((compressedData[(int)lVar6] >> param_3) & 1);
-                        uVar8 = MemoryMarshal.Read<uint>(allocated.Slice((int)((uVar7 * 0x10) + 8)));
-                        if (allocated[(int)((uVar8 * 0x10) + 0xc)] != bVar4)
+                        uVar8 = MemoryMarshal.Read<uint>(allocated.Slice((int)((uVar7 * 0x10) + 8 + 16)));
+                        if (allocated[(int)((uVar8 * 0x10) + 0xc + 16)] != bVar4)
                         {
-                            uVar8 = MemoryMarshal.Read<uint>(allocated.Slice((int)((uVar7 * 0x10) + 4)));
-                            if (allocated[(int)((uVar8 * 0x10) + 0xc)] != bVar4)
+                            uVar8 = MemoryMarshal.Read<uint>(allocated.Slice((int)((uVar7 * 0x10) + 4 + 16)));
+                            if (allocated[(int)((uVar8 * 0x10) + 0xc + 16)] != bVar4)
                             {
                                 break;
                             }
@@ -681,6 +697,7 @@ namespace MahoyoHDRepack
             }
             if (int2 != 0)
             {
+                allocated = allocated.Slice(16);
                 do
                 {
                     uVar10 = uVar11;
